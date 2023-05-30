1996ddc439ee7d24d65a41d0aebe5fc321ebe920
JALAL ABDESSAMAD <ABJALAL@royalairmaroc.com>
2023-05-30 16:09:04 +0100

    h

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,5 +2,17 @@
     <div class="col"><a type="button" class="btn btn-primary">Liste des Vélos</a>                    </div>
     <div class="col"><a type="button" class="btn btn-primary">Indisponible</a></div>
     <div class="col"><a type="button" class="btn btn-primary">Mon historique</a></div>
-    <div class="col"><a type="button" class="btn btn-primary">Administration</a></div>
+    <div class="col">
+        <div class="btn-group">
+            <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              Administration
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Liste des vélos</a></li>
+              <li><a class="dropdown-item" href="#">Ajouter un vélo</a></li>
+              <li><a class="dropdown-item" href="#">Liste des utilisateurs</a></li>
+              
+            </ul>
+          </div>
+    </div>
 </div>
